b16d77f3d014258505a46399ab74beeb354cd1b7
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2016-10-26 23:19:25 +0200

    refactored test

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
 (defproject rsimulatorclj-core "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simulates services"
+  :url "https://github.com/bjuvensjo/rsimulatorclj-core"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  ;; :user {:plugins [[cider/cider-nrepl "0.11.0snapshot"]]}
   :dependencies [
-                 [com.taoensso/timbre "4.2.0"]
-                 [org.clojure/clojure "1.7.0"]
+                 [com.taoensso/timbre "4.7.4"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.nrepl "0.2.12"]
                  [org.clojure/data.xml "0.0.8"]
                  ])
--- src/rsimulatorclj_core/Simulator.clj
+++ src/rsimulatorclj_core/Simulator.clj
@@ -1,30 +1,55 @@
 (ns rsimulatorclj-core.Simulator
-  (:require [clojure.data.xml :as xml]
-            [clojure.java.io :as io]
-            [taoensso.timbre :as timbre
-             :refer (log trace debug info warn error fatal report
-                         logf tracef debugf infof warnf errorf fatalf reportf
-                         spy get-env log-env)]))
+  (:require [clojure.test :refer [is]]
+            [taoensso.timbre :as timbre :refer [info warn]]
+            [clojure.data.xml :as xml]
+            [clojure.java.io :as io]))
 
-(defn normalize [request extension]
+(defn
+  ^{:doc  "Normalize the request appropriate for the extension"
+    :test (fn []
+            (is (= (normalize "foo" "txt") "foo"))
+            (is (= (normalize " foo  " "txt") " foo  "))
+            (is (= (normalize "<xml>\n<foo>foo</foo>   </xml>" "xml") "<xml><foo>foo</foo></xml>"))
+            (is (= (normalize "<xml> foo </xml>" "xml") "<xml> foo </xml>"))
+            )}
+  normalize [request extension]
   (case extension
     "xml" (.substring (xml/emit-str (xml/parse-str request)) 38) ;; Remove xml prolog and format
     request))
 
-(defn findMatch [rootPath rootRelativePath extension request]
+(defn
+  ^{:doc  "Tries to find a match for the specified request in the files <rootPath>/<rootRelativePath>/*<extension>."
+    :test (fn []
+            (is (= (:matches (findMatch "./test" "rsimulatorclj_core" "xml" "<xml>xml</xml>")) "<xml>xml</xml>"))
+            (is (= (:matches (findMatch "./test" "rsimulatorclj_core" "xml" "<xml><foo>foo</foo><bar>bar</bar></xml>")) '("<xml><foo>foo</foo><bar>bar</bar></xml>" "foo" "bar")))
+            (is (= (:matches (findMatch "./test" "rsimulatorclj_core" "xml" "<xml></xml>")) nil))
+            )} 
+  findMatch [rootPath rootRelativePath extension request]
   (let [matches (volatile! nil)]
     {:matchingRequestFile (first (filter #(and (.endsWith (.getName %) (str "Request." extension))
                                                (vreset! matches (re-matches (re-pattern (normalize (slurp %) extension)) (normalize request extension))))
                                          (file-seq (io/file (str rootPath "/" rootRelativePath)))))
      :matches @matches}))
 
-(defn getResponse [matchingRequestFile matches]
+(defn
+  ^{:doc  "Get the response that corresponds to the matchingRequestFile replaced with matches."
+    :test (fn []
+            (is (= (getResponse (io/file "./test/rsimulatorclj_core/Test1Request.xml") "<response>response</response>") "<response>response</response>\n"))
+            (is (= (getResponse (io/file "./test/rsimulatorclj_core/Test2Request.xml") '("<xml><foo>foo</foo><bar>bar</bar></xml>" "foo" "bar")) "<response>foobar</response>\n"))
+            )} 
+  getResponse [matchingRequestFile matches]
   ((reduce (fn [accum match] [(.replaceAll (accum 0) (str "[$]+[{]+" (accum 1) "[}]+") match) (inc (accum 1))])
            [(slurp (io/file (.getParent matchingRequestFile) (.replaceFirst (.getName matchingRequestFile) "Request" "Response"))) 1]
            (if (coll? matches) (next matches) '()))
    0))
 
-(defn getConfig [matchingRequestFile]  
+(defn
+  ^{:doc  "Get config that corresponds to the matchingRequestFile"
+    :test (fn []
+            (is (= (getConfig (io/file "./test/rsimulatorclj_core/Test1Request.xml")) nil))
+            (is (= (:foo (getConfig (io/file "./test/rsimulatorclj_core/Test2Request.xml"))) "foo"))
+            )} 
+  getConfig [matchingRequestFile]
   (let [configFile (io/file (.getParent matchingRequestFile) (.replaceFirst (.getName matchingRequestFile) "Request.*" "Config.clj"))]
     (if (.exists configFile)
       (read-string (slurp configFile)))))
@@ -36,9 +61,22 @@
 
 (defrecord SimulatorResponse [matchingRequestFile response config])
 
-(defn service [rootPath rootRelativePath request contentType]
+(defn
+  ^{:doc  "Returns a response of the specified request if such can be found recursively in <rootPath>/<rootRelativePath> in a request/response pair with the specified extension."
+    :test (fn []
+            (is (= (:response (service "./test/rsimulatorclj_core" "" "<xml></xml>" "xml")) nil))
+            (let [simulatorResponse (service "./test/rsimulatorclj_core" "" "<xml>xml</xml>" "xml")]
+              (is (= (.getName (:matchingRequestFile simulatorResponse)) "Test1Request.xml"))
+              (is (= (:response simulatorResponse) "<response>response</response>\n"))
+              (is (= (:properties simulatorResponse) nil)))
+            (let [simulatorResponse (service "./test/rsimulatorclj_core" "" "<xml><foo>foo</foo><bar>bar</bar></xml>" "xml")]
+              (is (= (.getName (:matchingRequestFile simulatorResponse)) "Test2Request.xml"))
+              (is (= (:response simulatorResponse) "<xml><response>foobar</response></xml>"))
+              (is (= (:foo (:config simulatorResponse)) "foo")))            
+            )}
+  service [rootPath rootRelativePath request contentType]
   (let [scriptMap {:rootPath rootPath :rootRelativePath rootRelativePath :request request :contentType contentType}
-        _ (info "entry:" scriptMap)
+        _         (info "entry:" scriptMap)
         scriptMap (applyScript scriptMap (io/file (str rootPath "/GlobalRequest.clj")))]
     (or (:simulatorResponse scriptMap)
         (let [{matchingRequestFile :matchingRequestFile matches :matches} (findMatch rootPath rootRelativePath ({:txt "txt" :xml "xml"} (keyword contentType)) request)]
@@ -49,4 +87,4 @@
                                 (applyScript (io/file (str rootPath "/GlobalResponse.clj"))))]
               (info "exit:" scriptMap)
               (:simulatorResponse scriptMap))
-            (info "exit: No response found" scriptMap))))))
+            (warn "exit: No response found" scriptMap))))))
